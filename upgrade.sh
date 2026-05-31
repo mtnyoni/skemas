5a48e7456ab9529461f9ccfbdcb6cda081745889
Tawanda M Nyoni <tmnyoni@outlook.com>
2026-05-31 14:51:59 +0200

    update upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -4,6 +4,18 @@ set -e
 SPEC="skemas.spec"
 SOURCE_DIR=$(rpm --eval '%{_sourcedir}')
 
+# If a version argument is given, update the spec. Otherwise auto-increment Release.
+if [ -n "$1" ]; then
+    sed -i "s/^Version:.*/Version:        $1/" "$SPEC"
+    sed -i "s/^Release:.*/Release:        1%{?dist}/" "$SPEC"
+    echo "==> Version set to $1"
+else
+    CURRENT=$(grep '^Release:' "$SPEC" | grep -o '[0-9]\+')
+    NEXT=$((CURRENT + 1))
+    sed -i "s/^Release:.*/Release:        $NEXT%{?dist}/" "$SPEC"
+    echo "==> Release bumped to $NEXT"
+fi
+
 echo "==> Building skemas..."
 odin build . -collection:shared=vendor -o:speed
 
